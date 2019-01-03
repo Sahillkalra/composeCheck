b8c86d89906649c9eba9a2c77bb040e231eb41b5
Sahillkalra <prognist@gmail.com>
2019-01-03 17:46:06 +0530

    Update http-concept.js

--- http-concept.js
+++ http-concept.js
@@ -1,4 +1,39 @@
-const fetch = require('node-fetch'); 
+const fetch = require('node-fetch');
+yaml = require('js-yaml');
+fs   = require('fs');
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+function versionExtract(value){
+    let res = value.split("/");
+    console.log(res);
+    for( index=0; index < res.length; index++){
+            if (res[index].search("jenkins") !=-1)
+            return res[index];
+    }
+
+}
+
+function updateDockerYml(value){
+    try {
+        var doc = yaml.safeLoad(fs.readFileSync('docker-compose.yml', 'utf8')); 
+        
+        //let indentedJson = JSON.stringify(doc, null, 4);
+        //console.log(indentedJson);  
+       //let value = doc.services.mongodb.image;
+        //console.log(value);
+       // let arr = value.split(":");
+       // arr[1] = "1.2.2";
+        //var final_value = arr.join(":");
+        //console.log(final_value);
+        //doc.services.mongodb.image = arr.join(":");
+        doc.services.mongodb.image = value;        
+        fs.writeFileSync('docker-compose.yml',yaml.dump(doc));            
+       console.log("-------------- Done ----------------");
+      } catch (e) {
+        console.log(e);
+      }
+}
 
 
 const uri = 'https://bin.barco.com/artifactory/api/storage/evm-containeros-local?lastModified';
@@ -14,6 +49,7 @@ let req = new fetch.Request(uri, {
 });
 
 
+
 fetch(req)    
     .then((res) => {
             if(res.ok){
@@ -27,5 +63,7 @@ fetch(req)
     })
     .then(json => {
         console.log("--------------------------------------------")
-        console.log(json)})
+        let value = json['uri'];
+        updateDockerYml(versionExtract(value));
+        })
     .catch(err => console.error(err));
